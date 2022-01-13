76317efe8689ee6c2d6b5bfbf126af98281045db
eunji-seo <toma1019@gmail.com>
2022-01-13 14:10:35 -0800

    커밋

---
+++ src/main/java/com/test/quiz/DatabaseQuiz01Delete.java
@@ -0,0 +1,44 @@
+package com.test.quiz;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.test.common.MysqlService;
+
+@WebServlet("/db/quiz01_delete")
+public class DatabaseQuiz01Delete extends HttpServlet{
+	
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		//쿼리 파라미터 id값 얻는다 
+			int id = Integer.valueOf(request.getParameter("id"));
+		//DB 연결
+			MysqlService mysql = MysqlService.getInstance();
+			mysql.connection();
+		
+		//id 값으로 delete query 수행
+			
+			String deleteQuery = "delete from `email` where `id` = " + id;
+			
+			try {
+				mysql.update(deleteQuery);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		
+		//DB 연결해제
+			
+			mysql.disconnection();
+		
+		//	리다이렉트 ->quiz01
+			
+			response.sendRedirect("/lesson04/quiz01/quiz01.jsp");
+	}
+	
+
+}
---
+++ src/main/java/com/test/quiz/Databasequiz01Insert.java
@@ -0,0 +1,43 @@
+package com.test.quiz;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.test.common.MysqlService;
+
+@WebServlet("/db/quiz01_insert")
+public class Databasequiz01Insert extends HttpServlet {
+	
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		
+		//폼에 있는 값들을 꺼낸다
+			String name = request.getParameter("name");
+			String url = request.getParameter("url");
+			
+		//BD 연결
+			MysqlService mysql = MysqlService.getInstance();
+			mysql.connection();
+		
+		//email 테이블 insert
+			String insertQuery = "insert into `email`(`name`,`url`)"
+					+ "values('"+ name +"','"+ url +"')";
+
+			try {
+				mysql.update(insertQuery);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		
+		// DB 연결 해제
+			mysql.disconnection();
+		// 리다이렉트 목록 화면
+			response.sendRedirect("/lesson04/quiz01/quiz01.jsp");
+	}
+
+}
